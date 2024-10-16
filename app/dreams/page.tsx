32cd5f9bebd1ef7ecdd980ed074fa1ed99034cd9
Alex <alex.luxfero@gmail.com>
2024-10-16 15:16:03 +0500

    init data log

--- app/dreams/page.tsx
+++ app/dreams/page.tsx
@@ -1,57 +1,7 @@
 "use client";
-
-import {
-  hideBackButton,
-  isBackButtonMounted,
-  isBackButtonSupported,
-  isInvoiceOpened,
-  mountBackButton,
-  offBackButtonClick,
-  onBackButtonClick,
-  openInvoice,
-  showBackButton,
-} from "@telegram-apps/sdk-react";
 import Link from "next/link";
-import { useEffect } from "react";
-import { usePathname, useRouter } from "next/navigation";
-import { createInvoiceLink } from "@/api/invoice";
 
 export default function Dreams() {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (isBackButtonSupported()) {
-      const handleBackButtonClick = () => {
-        if (pathname !== "/") {
-          router.push("/");
-        }
-      };
-      if (!isBackButtonMounted()) {
-        mountBackButton();
-      }
-
-      showBackButton();
-      onBackButtonClick(handleBackButtonClick);
-
-      return () => {
-        offBackButtonClick(handleBackButtonClick);
-        hideBackButton();
-      };
-    }
-  }, [pathname, router]);
-
-  const startPay = async () => {
-    const invoiceLink = await createInvoiceLink();
-    if (invoiceLink) {
-      isInvoiceOpened(); // false
-      const promise = openInvoice(invoiceLink, "url");
-      isInvoiceOpened(); // true
-      const status = await promise;
-      console.log(status);
-    }
-  };
-
   return (
     <div>
       <h1>Dreams</h1>
@@ -60,7 +10,6 @@ export default function Dreams() {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita,
         quas?
       </p>
-      <button onClick={() => startPay()}>Купить</button>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 import Link from "next/link";
+import { useLaunchParams } from "@telegram-apps/sdk-react";
+import { useEffect } from "react";
 
 export default function Home() {
+  const initData = useLaunchParams();
+  useEffect(() => {
+    console.log(initData);
+  }, [initData]);
   return (
     <div>
       <h1>Home</h1>
