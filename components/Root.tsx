c0e7b9de7de6a59e57f4c907cccd07f81fd2898f
Alex <alex.luxfero@gmail.com>
2024-10-28 22:46:52 +0500

    test

--- components/Root.tsx
+++ components/Root.tsx
@@ -12,9 +12,6 @@ import {
   isViewportStable,
   isViewportMounted,
   isViewportExpanded,
-  viewportHeight,
-  viewportStableHeight,
-  viewportState,
 } from "@telegram-apps/sdk-react";
 import { useTelegramMock } from "@/hooks/useTelegramMock";
 import { useBackButton } from "@/hooks/useBackButton";
