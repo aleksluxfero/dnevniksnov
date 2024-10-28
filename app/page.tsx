531dcd5d32dbd93a7b09a7f76fbdcc3da40ab2d9
Alex <alex.luxfero@gmail.com>
2024-10-29 00:22:48 +0500

    test page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import { useLaunchParams } from "@telegram-apps/sdk-react";
 import { useEffect } from "react";
-import { Loader } from "@/components/loader/app-loader";
 
 export default function Home() {
   const initData = useLaunchParams();
@@ -10,12 +9,9 @@ export default function Home() {
     console.log(initData);
   }, [initData]);
 
-  if (!initData) {
-    return <Loader />;
-  }
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home {initData.initData?.user?.username}</h1>
       <Link href={"/dreams"}>Сны</Link>
       <Link href={"/test-invoice"}>Покупка</Link>
     </div>
