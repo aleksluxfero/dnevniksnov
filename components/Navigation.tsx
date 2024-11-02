00b3fb6d18a4b901f4b3e346c0fe4388db1a6723
Alex <aleks.luxfero@gmail.com>
2024-11-02 13:45:04 +0500

    test nav colors

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -43,7 +43,7 @@ const menuNav = [
 export const Navigation = () => {
   return (
     <div className="fixed w-full bottom-0 left-0">
-      <div className="px-4 py-2 flex items-center justify-between border-t border-t-[#1f2937] bg-background">
+      <div className="px-4 py-2 flex items-center justify-between border-t border-t-card-foreground bg-background">
         {menuNav.map((menu) => {
           return (
             <div key={menu.alias}>
