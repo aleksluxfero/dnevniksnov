7b34032289ed978da167f5428de2cb3c3ca8acb4
Alex <aleks.luxfero@gmail.com>
2024-11-14 15:31:26 +0500

    change line height

--- components/DreamCard.tsx
+++ components/DreamCard.tsx
@@ -29,15 +29,18 @@ export const DreamCard: FC<DreamCardProps> = ({ dream }) => {
       {(dream.title || dream.description) && (
         <CardContent className="p-0 px-2 flex flex-col gap-2">
           {dream.title && (
-            <CardTitle className="p-0 text-lg line-clamp-1">
+            <CardTitle className="p-0 text-lg leading-5 line-clamp-1">
               {dream.title}
             </CardTitle>
           )}
           <p
             onClick={() => setIsOpenDescription((prevState) => !prevState)}
-            className={cn("line-clamp-3 text-base m-0 text-[#e7e8ec]", {
-              "line-clamp-none": isOpenDescription,
-            })}
+            className={cn(
+              "line-clamp-3 text-base leading-5 m-0 text-[#e7e8ec]",
+              {
+                "line-clamp-none": isOpenDescription,
+              },
+            )}
           >
             {dream.description}
           </p>
