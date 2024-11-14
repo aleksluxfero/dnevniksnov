fb898cae4605ed581897c6eaf7eb3ae72eb6ec2c
Alex <aleks.luxfero@gmail.com>
2024-11-14 18:34:46 +0500

    add collapse text

--- components/DreamCard.tsx
+++ components/DreamCard.tsx
@@ -2,15 +2,50 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Dream } from "@/types";
 import { FC, useState } from "react";
 import { cn } from "@/lib/utils";
+import { Link } from "@/components/Link/Link";
 
 interface DreamCardProps {
   dream: Dream;
 }
 
 export const DreamCard: FC<DreamCardProps> = ({ dream }) => {
-  const [isOpenDescription, setIsOpenDescription] = useState(false);
+  const [isOpenText, setIsOpenText] = useState(false);
+
+  const getText = (description: string, id: string) => {
+    if (!isOpenText && description.length > 130) {
+      return (
+        <p>
+          {description.slice(0, 120).trimEnd() + "... "}
+          <Link
+            href={"#" + id}
+            className="text-[#1886ff]"
+            onClick={() => setIsOpenText(true)}
+          >
+            Показать еще
+          </Link>
+        </p>
+      );
+    }
+    if (isOpenText) {
+      return (
+        <p>
+          {description + "... "}
+          <Link
+            href={"#" + id}
+            className="text-[#1886ff]"
+            onClick={() => setIsOpenText(false)}
+          >
+            Скрыть
+          </Link>
+        </p>
+      );
+    }
+    return description;
+  };
+
   return (
     <Card
+      id={dream.id}
       className={cn(
         "w-full bg-[#191a22] text-[#f2f3f5] border-none flex flex-col gap-2",
         {
@@ -29,21 +64,19 @@ export const DreamCard: FC<DreamCardProps> = ({ dream }) => {
       {(dream.title || dream.description) && (
         <CardContent className="p-0 px-2 flex flex-col gap-2">
           {dream.title && (
-            <CardTitle className="p-0 text-lg leading-5 line-clamp-1">
+            <CardTitle className="p-0 text-lg leading-5">
               {dream.title}
             </CardTitle>
           )}
-          <p
-            onClick={() => setIsOpenDescription((prevState) => !prevState)}
-            className={cn(
-              "line-clamp-3 text-base leading-5 m-0 text-[#e7e8ec]",
-              {
-                "line-clamp-none": isOpenDescription,
-              },
-            )}
-          >
-            {dream.description}
-          </p>
+          {dream.description && (
+            <div
+              className={cn(
+                "text-base leading-5 m-0 text-[#e7e8ec] cursor-pointer",
+              )}
+            >
+              {getText(dream.description, dream.id)}
+            </div>
+          )}
         </CardContent>
       )}
     </Card>
